798731e59d7273b66db1948b1e3855086fdf9b67
Piyush Jakhar <piyushjakhar08@gmail.com>
2021-05-26 10:42:50 +0530

    Add Login and Signup pages

--- api/db/index.js
+++ api/db/index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const URL = "mongodb+srv://test:test@cluster0.awut9.mongodb.net/RoomRentSikar?retryWrites=true&w=majority";
-
+const URL = "mongodb://test:test@cluster0-shard-00-00.awut9.mongodb.net:27017,cluster0-shard-00-01.awut9.mongodb.net:27017,cluster0-shard-00-02.awut9.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-1m9f1y-shard-0&authSource=admin&retryWrites=true&w=majority"
 const parameters = {
     useNewUrlParser: true,
     useUnifiedTopology: true
--- api/index.js
+++ api/index.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const keys = require('./config/keys');
 
 
 //Db Setup
@@ -16,22 +16,22 @@ require('./models');
 var port = 4000;
 var app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
+// passport.use(new GoogleStrategy({
+//     clientID: keys.googleClientID,
+//     clientSecret: keys.googleClientSecret,
+//     callbackURL: '/auth/google/callback'
+// }, (accessToken, refreshToken, profile, done) => {
 
-    console.log('profile', profile);
-})
-);
+//     console.log('profile', profile);
+// })
+// );
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-})
-);
+// app.get('/auth/google', passport.authenticate('google', {
+//     scope: ['profile', 'email']
+// })
+// );
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+// app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 app.set('port', port);
